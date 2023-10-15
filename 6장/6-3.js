363f870dddea098065922a5f3632cac1f87a28b9
shinbeomsik <tnghrltkwnst@naver.com>
2023-10-15 18:17:11 +0900

    6-3장 블로그정리

--- 6장/6-3.js
+++ 6장/6-3.js
@@ -1,7 +1,10 @@
 const box = document.querySelector("#box");
 
+// .style을 붙히고 변경하고 싶은 속성명을 입력 
 box.style.width = "10%";
 box.style.height = "300px";
 box.style.backgroundColor = "hotpink";
+//         └background-color처럼 단어 2개가 -으로 연결되어 있을때는 
+//          카멜케이스로 입력
 box.style.border = "none";
 box.style.transform = "rotate(30deg)";
