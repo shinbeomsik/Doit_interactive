88913f2bdec132bba502b4885f0350659457b963
shinbeomsik <tnghrltkwnst@naver.com>
2023-10-16 09:33:42 +0900

    실습페이지 만드는중

---
+++ 사이트1/ex1.js
@@ -0,0 +1,60 @@
+// const ex1sample = document.getElementById("ex1sample");
+
+// ex1sample.addEventListener("mouseenter", () => {
+//     ex1sample.style.animationPlayState = "paused"; // 마우스가 요소 위로 올라갈 때 애니메이션 시작
+// });
+
+// ex1sample.addEventListener("mouseleave", () => {
+//     ex1sample.style.animationPlayState = "running"; // 마우스가 요소에서 벗어날 때 애니메이션 일시 중지
+// });
+
+
+// const ex2sample = document.getElementById("ex2sample");
+
+// ex2sample.addEventListener("mouseenter", () => {
+//     ex2sample.style.animationPlayState = "paused"; // 마우스가 요소 위로 올라갈 때 애니메이션 시작
+// });
+
+// ex2sample.addEventListener("mouseleave", () => {
+//     ex2sample.style.animationPlayState = "running"; // 마우스가 요소에서 벗어날 때 애니메이션 일시 중지
+// });
+
+// const ex3sample = document.getElementById("ex3sample");
+
+// ex3sample.addEventListener("mouseenter", () => {
+//     ex3sample.style.animationPlayState = "paused"; // 마우스가 요소 위로 올라갈 때 애니메이션 시작
+// });
+
+// ex3sample.addEventListener("mouseleave", () => {
+//     ex3sample.style.animationPlayState = "running"; // 마우스가 요소에서 벗어날 때 애니메이션 일시 중지
+// });
+
+// const ex4sample = document.getElementById("ex4sample");
+
+// ex4sample.addEventListener("mouseenter", () => {
+//     ex4sample.style.animationPlayState = "paused"; // 마우스가 요소 위로 올라갈 때 애니메이션 시작
+// });
+
+// ex4sample.addEventListener("mouseleave", () => {
+//     ex4sample.style.animationPlayState = "running"; // 마우스가 요소에서 벗어날 때 애니메이션 일시 중지
+// });
+
+// const ex5sample = document.getElementById("ex5sample");
+
+// ex5sample.addEventListener("mouseenter", () => {
+//     ex5sample.style.animationPlayState = "paused"; // 마우스가 요소 위로 올라갈 때 애니메이션 시작
+// });
+
+// ex5sample.addEventListener("mouseleave", () => {
+//     ex5sample.style.animationPlayState = "running"; // 마우스가 요소에서 벗어날 때 애니메이션 일시 중지
+// });
+
+// const ex6sample = document.getElementById("ex6sample");
+
+// ex6sample.addEventListener("mouseenter", () => {
+//     ex6sample.style.animationPlayState = "paused"; // 마우스가 요소 위로 올라갈 때 애니메이션 시작
+// });
+
+// ex6sample.addEventListener("mouseleave", () => {
+//     ex6sample.style.animationPlayState = "running"; // 마우스가 요소에서 벗어날 때 애니메이션 일시 중지
+// });
