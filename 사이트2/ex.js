53f49daa14499d0e7cd7797819442c7c2cd63749
shinbeomsik <tnghrltkwnst@naver.com>
2023-10-26 10:52:46 +0900

    설명 해야하는 부분 주석 달아놈

--- 사이트2/ex.js
+++ 사이트2/ex.js
@@ -22,7 +22,8 @@ const list = document.querySelector(".list");
 const items = list.children;
 const btn1 = document.querySelector(".btn1");
 
-btn1.addEventListener("click" , ()=>{
+/* i = 1부터하면 첫번째는 콘솔에 찍히지 않음*/  
+btn1.addEventListener("click" , ()=>{        
     for(let i=0; i<items.length;i++){
         console.log(items[i].textContent);
     }
@@ -48,7 +49,7 @@ const btnMinus = document.querySelector("#minus1");
 let num =0;
 
 btnPlus.addEventListener("click" , e=>{
-    e.preventDefault();
+    e.preventDefault();  /* preventDefault()은 기본 이벤트 동작을 중단시킴 */
     num++;
     console.log(num);
 })
@@ -70,13 +71,13 @@ btnMinus.addEventListener("click" , e=>{
 const btnLeft = document.querySelector("#btnLeft");
 const btnRight = document.querySelector("#btnRight");
 const box2 = document.querySelector("#box2");
-const deg = 45;
-let num2 =0;
+const deg = 45;  /*각도 정하는 변수*/
+let num2 =0;  /* 회전 수 */
 
 btnLeft.addEventListener("click" , e=>{
     e.preventDefault();
     num2--;
-    box2.style.transform=`rotate(${num2*deg}deg)`;
+    box2.style.transform=`rotate(${num2*deg}deg)`;  /* rotateX 라고 바꾸거나 rotateY 라고 바꾸어도 상관없음  */ 
 })
 btnRight.addEventListener("click" , e=>{
     e.preventDefault();
@@ -93,9 +94,9 @@ ex5.addEventListener("click",()=>{
     let isOn1 = ex5.classList.contains("on");
     console.log(isOn1);
 
-    if(isOn1){
+    if(isOn1){ /* isOn1이 true 이면 실행 */ 
         ex5.classList.remove("on");
-    }else {
+    }else { /* isOn1이 false 이면 실행 */ 
         ex5.classList.add("on");
     }
 });
